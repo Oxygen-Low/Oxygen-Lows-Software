e4bc416609ccd151fc85395fb8f56635126490cb
Oxygen-Low <info@danielward.xyz>
2026-07-29 19:54:09 +0100

    Typecheck fixes.

--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+declare global {
+  interface Window {
+    chrome?: {
+      webview?: {
+        addEventListener: (type: string, listener: (event: any) => void) => void;
+        removeEventListener: (type: string, listener: (event: any) => void) => void;
+        postMessage: (message: any) => void;
+      };
+    };
+  }
+}
+
+export {};
+
