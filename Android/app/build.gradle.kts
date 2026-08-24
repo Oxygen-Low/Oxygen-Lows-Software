024df24b1f0467b91442025b43073cf543ad5099
Oxygen-Low <info@danielward.xyz>
2026-08-24 13:53:15 +0100

    Fix android update

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.File
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -18,9 +20,31 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            val keystorePath = System.getenv("KEYSTORE_PATH") ?: project.findProperty("KEYSTORE_PATH") as? String
+            val keyStoreFile = when {
+                keystorePath == null -> null
+                file(keystorePath).exists() -> file(keystorePath)
+                rootProject.file(keystorePath).exists() -> rootProject.file(keystorePath)
+                File(keystorePath).exists() -> File(keystorePath)
+                else -> null
+            }
+
+            if (keyStoreFile != null) {
+                storeFile = keyStoreFile
+                storePassword = System.getenv("KEYSTORE_PASSWORD") ?: project.findProperty("KEYSTORE_PASSWORD") as? String
+                keyAlias = System.getenv("KEY_ALIAS") ?: project.findProperty("KEY_ALIAS") as? String
+                keyPassword = System.getenv("KEY_PASSWORD") ?: project.findProperty("KEY_PASSWORD") as? String
+            } else {
+                initWith(getByName("debug"))
+            }
+        }
+    }
+
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             optimization {
                 enable = false
             }
